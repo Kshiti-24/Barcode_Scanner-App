a621eda3b93442486e2365fe02d78945160d8ff4
Kshitiz <kshitizagarwa2405@gmail.com>
2023-09-06 01:02:30 +0530

    first commit

---
+++ lib/screens/scanning_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ScanningScreen extends StatefulWidget {
+  const ScanningScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ScanningScreen> createState() => _ScanningScreenState();
+}
+
+class _ScanningScreenState extends State<ScanningScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
